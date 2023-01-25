90afa1e453abe201c251bb21f0221832c34ec851
Gerardin Clement <gclement@z1r4p3.42lyon.fr>
2023-01-25 13:42:30 +0100

    ecrit la deuxieme commande dans le fichier mais sans prendre en compte le res de la premiere

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gclement <gclement@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 10:08:43 by gclement          #+#    #+#             */
-/*   Updated: 2023/01/25 08:52:53 by gclement         ###   ########.fr       */
+/*   Updated: 2023/01/25 13:40:57 by gclement         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,26 @@ int	main(int argc, char	*argv[], char *envp[])
 	char	**cmd;
 	char	**argxec;
 	char	**all_path;
+	pid_t	pid;
+	int		fd[2];
 
 	(void) argc;
+	pid = fork();
+	if (pipe(fd) == -1)
+		exit (0);
+	if (pid == -1)
+		exit(EXIT_FAILURE);
 	all_path = create_array_path(envp);
-	cmd = ft_split(argv[2], ' ');
-	argxec = set_argxec(cmd, argv[1]);
-	call_cmd(argxec, cmd, all_path);
-	cmd = ft_split(argv[3], ' ');
-	argxec = set_argxec(cmd, argv[5]);
-	call_cmd(argxec, cmd, all_path);
+	if (pid == 0)
+		child(fd, argv, all_path);
+	else
+	{
+		close(STDOUT_FILENO);
+		dup2(fd[1], 1);
+		cmd = ft_split(argv[2], ' ');
+		argxec = set_argxec(cmd, argv[1]);
+		call_cmd(argxec, cmd, all_path);
+	}
 	free_2d_array(all_path);
 	exit (EXIT_SUCCESS);
 }
--- src/pipex_utils.c
+++ src/pipex_utils.c
@@ -6,7 +6,7 @@
 /*   By: gclement <gclement@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 10:04:34 by gclement          #+#    #+#             */
-/*   Updated: 2023/01/25 09:30:00 by gclement         ###   ########.fr       */
+/*   Updated: 2023/01/25 13:41:50 by gclement         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,20 +35,34 @@ char	**set_argxec(char **cmd, char *filename)
 	return (argxec);
 }
 
+char	**set_argxec_child(char **cmd)
+{
+	char	**argxec;
+	int		x;
+
+	x = 0;
+	while (cmd[x])
+		x++;
+	argxec = malloc((x + 1) * sizeof(char *));
+	if (!argxec)
+		exit (EXIT_FAILURE);
+	x = 0;
+	while (cmd[x])
+	{
+		argxec[x] = cmd[x];
+		x++;
+	}
+	argxec[x] = NULL;
+	return (argxec);
+}
+
 void	call_cmd(char **argxec, char **cmd, char **all_path)
 {
 	char	*path;
 	char	*tmp;
-	int		fd[2];
-	int		out;
 	int		x;
 
 	x = 0;
-	out = open("outfile", O_WRONLY | O_RDONLY);
-	if (pipe(fd) == -1)
-		exit (0);
-	close(fd[0]);
-	close(fd[1]);
 	while (all_path[x])
 	{
 		tmp = ft_strjoin(all_path[x], "/");
@@ -74,4 +88,21 @@ char	**create_array_path(char *envp[])
 		i++;
 	all_path = ft_split(ft_strchr(envp[i], '='), ':');
 	return (all_path);
+}
+
+void	child(int fd[2], char **argv, char **all_path)
+{
+	char	**cmd;
+	char	**argxec;
+	int		out;
+
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(fd[1]);
+	dup(fd[0]);
+	out = open(argv[4], O_WRONLY | O_RDONLY);
+	cmd = ft_split(argv[3], ' ');
+	argxec = set_argxec_child(cmd);
+	call_cmd(argxec, cmd, all_path);
+    close(fd[0]);
 }
